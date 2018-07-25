a48dcb81ed431792b4c430331bb53237a62c5766
Gary A. Newsome <garyanewsome@gmail.com>
2018-07-25 15:28:50 -0400

    Initial Commits

---
+++ README.md
@@ -0,0 +1,22 @@
+# Kaelber
+
+### - Site preview -
+[https://savvysoftworks.com/kaelberband](https://savvysoftworks.com/kaelberband)
+
+### - Needs -
+
+- [ ] Social Media accounts?
+- [ ] Calendar creds
+
+### - The how and to... -
+
+This site is built using Hugo [https://gohugo.io/](https://gohugo.io/)
+
+To run the site locally: `hugo server` and visit `http://localhost:1313`
+
+To Build the site: `hugo` (needs done before push)
+
+The site builds to the `docs` folder which in turns feeds the `gh-pages` branch
+
+
+
---
+++ static/js/index.js
@@ -0,0 +1,64 @@
+$(function() {
+  // jQuery Scroll Animations
+  $('a[href^="#"]').on('click', function(event) {
+    var target = $(this.getAttribute('href'))
+    if(target.length) {
+        event.preventDefault();
+        $('html, body').animate({
+            scrollTop: target.offset().top - 56
+        }, 500)
+    }
+  })
+
+  // on scroll
+  $(document).scroll(function () {
+    if ($(window).scrollTop() >= 24) {
+      $(".icons-return").fadeIn(2000)
+    } else {
+      $(".icons-return").fadeOut(1000)
+    }
+  })
+
+  // splash overlay
+  $.fn.center = function () {
+    this.css("position","absolute")
+    return this
+  }
+  $("#overlay").show()
+  $("#overlay-content").show().center()
+  setTimeout(function(){    
+    $("#overlay").fadeOut( 1600, "linear" )
+  }, 500)
+
+  // gallery
+  $(".filter-button").click(function(){
+    var value = $(this).attr('data-filter')
+
+    if(value == "all") {
+      $('.filter').show('1000');
+    } else {
+      $(".filter").not('.'+value).hide('2000')
+      $('.filter').filter('.'+value).show('2000')
+    }
+  })
+
+  var apiKey = ""
+  var calendarId = ""
+  $('#calendar').fullCalendar({
+      googleCalendarApiKey: apiKey,
+      timezone: 'New York',
+      events: {
+          googleCalendarId: calendarId
+      },
+      eventRender: function(event, element) {
+        element.qtip({
+          content: event.title,
+          style: 'qtip-bootstrap',
+          position: {
+            my: 'bottom center',
+            at: 'top left'
+          }
+      })
+    }
+  })
+})
